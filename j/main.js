3ebdf7f0211fe1c3076fb3c26fd11852255987e0
Aaron Gustafson <aaron@easy-designs.net>
2018-08-23 16:49:59 -0700

    Initial commit

---
+++ j/main.js
@@ -0,0 +1,33 @@
+if ('Windows' in window)
+{
+	Windows.UI.WebUI.addEventListener('suspending', pause);
+	Windows.UI.WebUI.addEventListener('suspending', resume);
+}
+
+var $output = document.getElementsByTagName('output')[0],
+		current = 0,
+		next = 1,
+		uri;
+
+function add( text ){
+	$output.innerHTML += text + '\r\n';
+}
+
+function pause(){
+	add('pause');
+}
+
+function resume(){
+	add('pause');
+}
+
+setTimeout(function(){
+	add(current);
+	current = next;
+	next = current + next;
+	uri = 'LICENSE?c=' + current + '&n=' + next;
+	fetch(new Request(uri)).then(function(){
+		add('fetched '+uri);
+	});
+},
+500);
